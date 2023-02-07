158cff75d0148fca8e4e221a225366106aa3ed5d
David Wolgemuth <davidjwolgemuth@gmail.com>
2023-02-07 13:45:03 -0500

    format files; some more styling

--- app.js
+++ app.js
@@ -1,173 +1,174 @@
-
-const RADIUS = 24;
+const RADIUS = 64;
 const CENTER = RADIUS + 1;
 const GRID_SIZE = RADIUS * 2 + 1;
 
-
-const TICK_RATE = 50
+const TICK_RATE = 10;
 
 class GameApp {
-    constructor() {
-        this.game = new Game()
-        document.addEventListener('keypress', e => this.onkeypress(e))
+  constructor() {
+    this.game = new Game();
+    document.addEventListener("keypress", (e) => this.onkeypress(e));
+  }
+
+  run() {
+    this.interval = setInterval(() => this.game_loop(), TICK_RATE);
+  }
+
+  game_loop() {
+    this.game.loop();
+    document.querySelector("#timer").innerHTML = `ticks: ${this.game.ticks}`;
+    this.update_display();
+  }
+
+  onkeypress(e) {
+    console.log(e);
+    switch (e.key) {
+      case "i": // UP
+        this.moveCursor(0, -1);
+        break;
+      case "l": // RIGHT
+        this.moveCursor(1, 0);
+        break;
+      case "j": // LEFT
+        this.moveCursor(-1, 0);
+        break;
+      case "k": // DOWN
+        this.moveCursor(0, 1);
+        break;
+    }
+  }
+
+  moveCursor(x, y) {
+    this.game.cursor_x += x;
+    this.game.cursor_y += y;
+    this.update_display();
+  }
+
+  get_grid_data() {
+    const data = [
+      // (x, y, label_id, tile, visible)
+    ];
+    for (let i = 0; i < GRID_SIZE; i += 1) {
+      for (let j = 0; j < GRID_SIZE; j += 1) {
+        // # if (i + j < 2) or ()
+        const x = this.game.cursor_x + i - RADIUS;
+        const y = this.game.cursor_y + j - RADIUS;
+
+        // # dist = abs(this.game.cursor_x - x) + abs(this.game.cursor_y - y)
+        // # visible = dist < RADIUS + 1
+        const label_id = `label-${i}-${j}`;
+        const tile = this.game.tiles.get(x, y);
+        data.push({
+          // # "visible": visible,
+          label_id: label_id,
+          tile: tile,
+        });
+      }
+    }
+    return data;
+  }
+
+  update_display() {
+    for (const tile_data of this.get_grid_data()) {
+      const label = document.querySelector("#" + tile_data["label_id"]);
+      this._update_tile_label(label, tile_data["tile"]);
     }
 
-    run() {
-        this.interval = setInterval(() => this.game_loop(), TICK_RATE);
+    this._update_tile_info(
+      document.querySelector("#tile-info"),
+      this.game.get_selected_tile()
+    );
+  }
+
+  _update_tile_label(label, tile) {
+    if (tile.cell) {
+      if (tile.cell.is_dead) {
+        label.style.background = "dimgrey";
+        label.innerHTML = "";
+      } else {
+        label.style.background = "lightseagreen";
+        label.innerHTML = "";
+        // `
+        //     w: ${tile.cell.water}
+        //     m: ${tile.cell.minerals}
+        //     c: ${tile.cell.carbon}
+        //     ns: ${tile.cell.sugar}
+        // `;
+      }
+    } else if (tile.type === Sky) {
+      label.style.background = "skyblue";
+      label.innerHTML = "";
+    } else if (tile.type === Dirt) {
+      label.style.background = "rosybrown";
+      label.innerHTML = "";
+    } else {
+      // invisible border
+      label.style.background = "white";
+      label.innerHTML = "";
     }
+  }
 
-    game_loop() {
-        this.game.loop()
-        document.querySelector("#timer").innerHTML = `ticks: ${this.game.ticks}`;
-        this.update_display()
-    }
-
-    onkeypress(e) {
-        console.log(e);
-        switch (e.key) {
-            case 'i': // UP
-                this.moveCursor(0, -1);
-                break;
-            case 'l': // RIGHT
-                this.moveCursor(1, 0);
-                break;
-            case 'j': // LEFT
-                this.moveCursor(-1, 0);
-                break;
-            case 'k': // DOWN
-                this.moveCursor(0, 1);
-                break;
-        }
-    }
-
-    moveCursor(x, y) {
-        this.game.cursor_x += x;
-        this.game.cursor_y += y
-        this.update_display();
-    }
-
-    get_grid_data() {
-        const data = [
-            // (x, y, label_id, tile, visible)
-        ]
-        for (let i = 0; i < GRID_SIZE; i += 1) {
-            for (let j = 0; j < GRID_SIZE; j += 1) {
-                // # if (i + j < 2) or ()
-                const x = this.game.cursor_x + i - RADIUS
-                const y = this.game.cursor_y + j - RADIUS
-
-                // # dist = abs(this.game.cursor_x - x) + abs(this.game.cursor_y - y)
-                // # visible = dist < RADIUS + 1
-                const label_id = `label-${i}-${j}`
-                const tile = this.game.tiles.get(x, y)
-                data.push({
-                    // # "visible": visible,
-                    "label_id": label_id,
-                    "tile": tile,
-                })
-            }
-        }
-        return data
-    }
-
-    update_display() {
-        for (const tile_data of this.get_grid_data()) {
-            const label = document.querySelector("#" + tile_data["label_id"])
-            this._update_tile_label(label, tile_data["tile"]);
-        }
-
-        this._update_tile_info(
-            document.querySelector("#tile-info"),
-            this.game.get_selected_tile(),
-        )
-    }
-
-    _update_tile_label(label, tile) {
-        if (tile.cell) {
-            if (tile.cell.is_dead) {
-                label.style.background = "dimgrey"
-                label.innerHTML = ""
-            } else {
-                label.style.background = "brown"
-                label.innerHTML ='';
-                // `
-                //     w: ${tile.cell.water}
-                //     m: ${tile.cell.minerals}
-                //     c: ${tile.cell.carbon}
-                //     ns: ${tile.cell.sugar}
-                // `;
-            }
-        } else if (tile.is_dirt) {
-            label.style.background = "rosybrown"
-            label.innerHTML = '';
-        } else {
-            label.style.background = "skyblue"
-            label.innerHTML = '';
-        }
-    }
-
-    _update_tile_info(data_table, tile) {
-        let html = `
+  _update_tile_info(data_table, tile) {
+    let html = `
             <dt>x</dt><dd>${tile.x}</dd>
             <dt>y</dt><dd>${tile.y}</dd>
-            <dt>dirt</dt><dd>${tile.is_dirt}</dd>
+            <dt>type</dt><dd>${tile.type}</dd>
             <dt>cell</dt><dd>----</dd>
         `;
-        if (tile.cell) {
-            html += `
+    if (tile.cell) {
+      html += `
                 <dt>age</dt><dd>${tile.cell.age}</dd>
                 <dt>water</dt><dd>${tile.cell.water}</dd>
                 <dt>minerals</dt><dd>${tile.cell.minerals}</dd>
                 <dt>carbon</dt><dd>${tile.cell.carbon}</dd>
                 <dt>sugar</dt><dd>${tile.cell.sugar}</dd>
             `;
-        }
-
-        data_table.innerHTML = `<dl>${html}</dl>`
     }
 
-    setupLayout() {
-        const tile_info_data_table = document.createElement('div')
-        tile_info_data_table.id = 'tile-info'
-        this._update_tile_info(tile_info_data_table, this.game.get_selected_tile())
+    data_table.innerHTML = `<dl>${html}</dl>`;
+  }
 
-        const container = document.createElement("div");
-        container.id = "container";
+  setupLayout() {
+    const tile_info_data_table = document.createElement("div");
+    tile_info_data_table.id = "tile-info";
+    this._update_tile_info(tile_info_data_table, this.game.get_selected_tile());
 
-        for (let i = 0; i < GRID_SIZE; i += 1) {
-            const row = document.createElement('div')
-            row.classList.add('row');
-            for (let j = 0; j < GRID_SIZE; j += 1) {
-                // # if (i + j < 2) or ()
-                const x = this.game.cursor_x + i - RADIUS
-                const y = this.game.cursor_y + j - RADIUS
+    const container = document.createElement("div");
+    container.id = "container";
 
-                // # dist = abs(this.game.cursor_x - x) + abs(this.game.cursor_y - y)
-                // # visible = dist < RADIUS + 1
-                const label_id = `label-${i}-${j}`
-                const tile = this.game.tiles.get(x, y)
+    for (let i = 0; i < GRID_SIZE; i += 1) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+      for (let j = 0; j < GRID_SIZE; j += 1) {
+        // # if (i + j < 2) or ()
+        const x = this.game.cursor_x + i - RADIUS;
+        const y = this.game.cursor_y + j - RADIUS;
 
-                const label = document.createElement("div");
-                label.classList.add('tile')
-                label.id = label_id
-                this._update_tile_label(label, tile)
+        // # dist = abs(this.game.cursor_x - x) + abs(this.game.cursor_y - y)
+        // # visible = dist < RADIUS + 1
+        const label_id = `label-${i}-${j}`;
+        const tile = this.game.tiles.get(x, y);
 
-                row.appendChild(label);
-            }
-            container.appendChild(row);
-        }
-        document.body.appendChild(container);
-        document.body.appendChild(tile_info_data_table);
+        const label = document.createElement("div");
+        label.classList.add("tile");
+        label.id = label_id;
+        this._update_tile_label(label, tile);
 
-        const timer = document.createElement("div");
-        timer.id = 'timer'
-        document.body.appendChild(timer);
+        row.appendChild(label);
+      }
+      container.appendChild(row);
     }
+    document.body.appendChild(container);
+    document.body.appendChild(tile_info_data_table);
+
+    const timer = document.createElement("div");
+    timer.id = "timer";
+    document.body.appendChild(timer);
+  }
 }
 
-
 document.addEventListener("DOMContentLoaded", () => {
-    const app = new GameApp();
-    app.setupLayout();
-    app.run();
-})
+  const app = new GameApp();
+  app.setupLayout();
+  app.run();
+});
--- game.js
+++ game.js
@@ -1,32 +1,39 @@
-
 const CardinalDirection = {
-  North: 'North',
-  South: 'South',
-  East: 'East',
-  West: 'West',
+  North: "North",
+  South: "South",
+  East: "East",
+  West: "West",
 };
 
 const Resource = {
-  Mineral: 'Mineral',
-  Sugar: 'Sugar',
-  Water: 'Water',
-  Carbon: 'Carbon',
+  Mineral: "Mineral",
+  Sugar: "Sugar",
+  Water: "Water",
+  Carbon: "Carbon",
 };
 
+const TileType = {
+  Dirt: "Dirt",
+  Sky: "Sky",
+  InvisibleBorder: "InvisibleBorder",
+}
 
 const { North, South, East, West } = CardinalDirection;
 const { Water, Sugar, Mineral, Carbon } = Resource;
+const { Dirt, Sky, InvisibleBorder } = TileType
 
-const MAX_WATER = 10
-const MAX_MINERALS = 10
-const MAX_CARBON = 10
-const MAX_SUGAR = 10
+const MAX_WATER = 10;
+const MAX_MINERALS = 10;
+const MAX_CARBON = 10;
+const MAX_SUGAR = 10;
 
 const DEATH_AGE = 240;
+const DETERIORATION_AGE = 480;
 
+const WIDTH = 64;
 
 class LifeCell {
-    // all resources max capacity of 10
+  // all resources max capacity of 10
   constructor({
     minerals,
     sugar,
@@ -59,70 +66,53 @@ class LifeCell {
   }
 
   check_can_reproduce() {
-    if (this.minerals < MAX_MINERALS / 2) { // 2:
-        return false
+    if (this.minerals < MAX_MINERALS / 2) {
+      // 2:
+      return false;
     }
-    if (this.sugar < MAX_SUGAR / 2) { // 2:
-        return false
+    if (this.sugar < MAX_SUGAR / 2) {
+      // 2:
+      return false;
     }
-    if (this.water < MAX_WATER / 2) { // 2:
-        return false
+    if (this.water < MAX_WATER / 2) {
+      // 2:
+      return false;
     }
-    if (this.carbon < MAX_CARBON / 2) { // 2:
-        return false
+    if (this.carbon < MAX_CARBON / 2) {
+      // 2:
+      return false;
     }
-    return true
+    return true;
   }
 
   reproduce() {
-    const cell = new LifeCell({})
+    const cell = new LifeCell({});
     cell.minerals = Math.floor(this.minerals / 2);
-    this.minerals = Math.floor(this.minerals / 2)
+    this.minerals = Math.floor(this.minerals / 2);
 
     cell.sugar = Math.floor(this.sugar / 2);
-    this.sugar = Math.floor(this.sugar / 2)
+    this.sugar = Math.floor(this.sugar / 2);
 
     cell.water = Math.floor(this.water / 2);
-    this.water = Math.floor(this.water / 2)
+    this.water = Math.floor(this.water / 2);
 
     cell.carbon = Math.floor(this.carbon / 2);
-    this.carbon = Math.floor(this.carbon / 2)
+    this.carbon = Math.floor(this.carbon / 2);
 
-    return cell
+    return cell;
   }
 }
 
-
 class Tile {
-  constructor({
-    x,
-    y,
-    is_dirt,
-    cell,
-  }) {
+  constructor({ x, y, type, cell }) {
     this.x = x;
     this.y = y;
-    this.is_dirt = is_dirt;
+    this.type = type;
     this.cell = cell;
   }
 
-  static build_dirt(x, y) {
-    return new Tile({
-      x: x,
-      y: y,
-      is_dirt: true,
-    });
-  }
-
-  static build_sky(x, y) {
-    return new Tile({
-      x: x,
-      y: y,
-    });
-  }
-
   adjacent_point(direction) {
-    switch(direction) {
+    switch (direction) {
       case North:
         return [this.x, this.y - 1];
       case South:
@@ -138,29 +128,30 @@ class Tile {
   }
 }
 
-
 class TileGrid {
   constructor() {
-    this.tiles = {}
+    this.tiles = {};
   }
 
   get(x, y) {
-    const key = `${x}_${y}`
+    const key = `${x}_${y}`;
     if (this.tiles[key]) {
       return this.tiles[key];
     } else {
       let tile;
-      if (y >= 0) {  // negative is north
-        tile = Tile.build_dirt(x, y);
+      if (Math.abs(x) > WIDTH / 2) {
+        tile = new Tile({ x, y, type: InvisibleBorder });
+      } else if (y >= 0) {
+        // negative is north
+        tile = new Tile({ x, y, type: Dirt });
       } else {
-        tile = Tile.build_sky(x, y);
+        tile = new Tile({ x, y, type: Sky });
       }
       this.tiles[key] = tile;
       return tile;
     }
   }
 }
-
 
 function max(a, b, key) {
   if (key(a) >= key(b)) {
@@ -176,7 +167,6 @@ function min(a, b, key) {
     return b;
   }
 }
-
 
 /**
  *
@@ -194,24 +184,30 @@ function min(a, b, key) {
  */
 class Game {
   constructor() {
-    this.cursor_x = 0
-    this.cursor_y = 0
-    this.ticks = 0
-    this.tiles = new TileGrid()
+    this.cursor_x = 0;
+    this.cursor_y = 0;
+    this.ticks = 0;
+    this.tiles = new TileGrid();
 
-    const seed = LifeCell.seed()
-    const seed_tile = Tile.build_dirt(0, 0)
-    seed_tile.cell = seed
-    this.tiles.tiles["0_0"] = seed_tile
+    const seed = LifeCell.seed();
+    const seed_tile = new Tile({ x: 0, y: 0, type: Dirt });
+    seed_tile.cell = seed;
+    this.tiles.tiles["0_0"] = seed_tile;
   }
 
   get_selected_tile() {
-    return this.tiles.get(this.cursor_x, this.cursor_y)
+    return this.tiles.get(this.cursor_x, this.cursor_y);
   }
 
   get_tiles_with_life_cells() {
-    return Object.values(this.tiles.tiles).filter(tile => {
-      return tile.cell && !tile.is_dead;
+    return Object.values(this.tiles.tiles).filter((tile) => {
+      return tile.cell && !tile.cell.is_dead;
+    });
+  }
+
+  get_tiles_with_dead_life_cells() {
+    return Object.values(this.tiles.tiles).filter((tile) => {
+      return tile.cell && tile.cell.is_dead;
     });
   }
 
@@ -240,13 +236,13 @@ class Game {
     other trees grow?...  MANY trees?
    */
   loop() {
-    const already_balanced = new Set()
+    const already_balanced = new Set();
 
     for (const tile of this.get_tiles_with_life_cells()) {
-      const moved_resources = new Set()
+      const moved_resources = new Set();
 
       for (const d of tile.cell.flow_direction) {
-        const adjacent_tile = this.tiles.get(...tile.adjacent_point(d))
+        const adjacent_tile = this.tiles.get(...tile.adjacent_point(d));
 
         if (adjacent_tile.cell) {
           const a = tile.cell;
@@ -254,7 +250,10 @@ class Game {
           const b = adjacent_tile.cell;
           const [bx, by] = [adjacent_tile.x, adjacent_tile.y];
 
-          const hash_value = `${Math.min(ax, bx)},${Math.min(ay, by)},${Math.max(ax, bx)},${Math.max(ay, by)}`
+          const hash_value = `${Math.min(ax, bx)},${Math.min(
+            ay,
+            by
+          )},${Math.max(ax, bx)},${Math.max(ay, by)}`;
 
           if (already_balanced.has(hash_value)) {
             // already balanced
@@ -263,77 +262,96 @@ class Game {
 
           let from_cell, to_cell;
 
-          from_cell = max(a, b, cell => cell.water)
-          to_cell = min(a, b, cell => cell.water)
-          if (!to_cell.is_dead && from_cell.water > 0 && to_cell.water < MAX_WATER & Math.abs(from_cell.water - to_cell.water) > 0) {
+          from_cell = max(a, b, (cell) => cell.water);
+          to_cell = min(a, b, (cell) => cell.water);
+          if (
+            !to_cell.is_dead &&
+            from_cell.water > 0 &&
+            (to_cell.water < MAX_WATER) &
+              (Math.abs(from_cell.water - to_cell.water) > 0)
+          ) {
             if (!moved_resources.has(Water)) {
-              from_cell.water -= 1
-              to_cell.water += 1
-              moved_resources.add(Water)
-              already_balanced.add(hash_value)
+              from_cell.water -= 1;
+              to_cell.water += 1;
+              moved_resources.add(Water);
+              already_balanced.add(hash_value);
             }
           }
 
-          from_cell = max(a, b, cell => cell.minerals)
-          to_cell = min(a, b, cell => cell.minerals)
-          if (!to_cell.is_dead && from_cell.minerals > 0 && to_cell.minerals < MAX_MINERALS & Math.abs(from_cell.minerals - to_cell.minerals) > 0) {
+          from_cell = max(a, b, (cell) => cell.minerals);
+          to_cell = min(a, b, (cell) => cell.minerals);
+          if (
+            !to_cell.is_dead &&
+            from_cell.minerals > 0 &&
+            (to_cell.minerals < MAX_MINERALS) &
+              (Math.abs(from_cell.minerals - to_cell.minerals) > 0)
+          ) {
             if (!moved_resources.has(Mineral)) {
-              from_cell.minerals -= 1
-              to_cell.minerals += 1
-              moved_resources.add(Mineral)
-              already_balanced.add(hash_value)
+              from_cell.minerals -= 1;
+              to_cell.minerals += 1;
+              moved_resources.add(Mineral);
+              already_balanced.add(hash_value);
             }
           }
 
-          from_cell = max(a, b, cell => cell.sugar)
-          to_cell = min(a, b, cell => cell.sugar)
-          if (!to_cell.is_dead && from_cell.sugar > 0 && to_cell.sugar < MAX_SUGAR & Math.abs(from_cell.sugar - to_cell.sugar) > 0) {
+          from_cell = max(a, b, (cell) => cell.sugar);
+          to_cell = min(a, b, (cell) => cell.sugar);
+          if (
+            !to_cell.is_dead &&
+            from_cell.sugar > 0 &&
+            (to_cell.sugar < MAX_SUGAR) &
+              (Math.abs(from_cell.sugar - to_cell.sugar) > 0)
+          ) {
             if (!moved_resources.has(Sugar)) {
-              from_cell.sugar -= 1
-              to_cell.sugar += 1
-              moved_resources.add(Sugar)
-              already_balanced.add(hash_value)
+              from_cell.sugar -= 1;
+              to_cell.sugar += 1;
+              moved_resources.add(Sugar);
+              already_balanced.add(hash_value);
             }
           }
 
-          from_cell = max(a, b, cell => cell.carbon)
-          to_cell = min(a, b, cell => cell.carbon)
-          if (!to_cell.is_dead && from_cell.carbon > 0 && to_cell.carbon < MAX_CARBON & Math.abs(from_cell.carbon - to_cell.carbon) > 0) {
+          from_cell = max(a, b, (cell) => cell.carbon);
+          to_cell = min(a, b, (cell) => cell.carbon);
+          if (
+            !to_cell.is_dead &&
+            from_cell.carbon > 0 &&
+            (to_cell.carbon < MAX_CARBON) &
+              (Math.abs(from_cell.carbon - to_cell.carbon) > 0)
+          ) {
             if (!moved_resources.has(Carbon)) {
-              from_cell.carbon -= 1
-              to_cell.carbon += 1
-              moved_resources.add(Carbon)
-              already_balanced.add(hash_value)
+              from_cell.carbon -= 1;
+              to_cell.carbon += 1;
+              moved_resources.add(Carbon);
+              already_balanced.add(hash_value);
             }
           }
-        } else if (adjacent_tile.is_dirt) {
+        } else if (adjacent_tile.type === Dirt) {
           if (tile.cell.minerals < MAX_MINERALS) {
             if (!moved_resources.has(Mineral)) {
-              tile.cell.minerals += 1
-              moved_resources.add(Mineral)
+              tile.cell.minerals += 1;
+              moved_resources.add(Mineral);
             }
           }
 
           if (tile.cell.water < MAX_WATER && d == South) {
             if (!moved_resources.has(Water)) {
-              tile.cell.water += 1
-              moved_resources.add(Water)
+              tile.cell.water += 1;
+              moved_resources.add(Water);
             }
           }
-
-        } else {
+        } else if (adjacent_tile.type === Sky) {
           // sky
           if (tile.cell.carbon < MAX_CARBON) {
             if (!moved_resources.has(Carbon)) {
-              tile.cell.carbon += 1
-              moved_resources.add(Carbon)
+              tile.cell.carbon += 1;
+              moved_resources.add(Carbon);
             }
           }
 
           if (tile.cell.sugar < MAX_SUGAR && d === North) {
             if (!moved_resources.has(Sugar)) {
-              tile.cell.sugar += 1
-              moved_resources.add(Sugar)
+              tile.cell.sugar += 1;
+              moved_resources.add(Sugar);
             }
           }
         }
@@ -344,21 +362,30 @@ class Game {
     //   reproduce tiles
     for (const tile of this.get_tiles_with_life_cells()) {
       if (tile.cell.check_can_reproduce()) {
-          for (const d of tile.cell.growth_direction) {
-            const [x, y] = tile.adjacent_point(d)
-            if (!this.tiles.get(x, y).cell) {
-              const new_cell = tile.cell.reproduce()
-              this.tiles.get(x, y).cell = new_cell
-              break;
-            }
+        for (const d of tile.cell.growth_direction) {
+          const [x, y] = tile.adjacent_point(d);
+          const adjacent_tile = this.tiles.get(x, y)
+          if (adjacent_tile.type !== InvisibleBorder && !adjacent_tile.cell) {
+            const new_cell = tile.cell.reproduce();
+            this.tiles.get(x, y).cell = new_cell;
+            break;
           }
         }
-
-        tile.cell.age += 1
       }
 
-    this.ticks += 1
+      tile.cell.age += 1;
+    }
+
+    // break down dead cells
+    for (const tile of this.get_tiles_with_dead_life_cells()) {
+      if (tile.cell.age === DETERIORATION_AGE) {
+        tile.cell = null;
+        tile.type = Dirt;
+      } else {
+        tile.cell.age += 1;
+      }
+    }
+
+    this.ticks += 1;
   }
 }
-
-
